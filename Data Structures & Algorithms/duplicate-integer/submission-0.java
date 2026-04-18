d0a40eb44333aa1a98abdebae5c8ce5748732fda
Bidisha Khatri <156510540+BidishaKhatri@users.noreply.github.com>
2026-04-18 19:04:19 +0900

    Update submission-0.java

--- Data Structures & Algorithms/duplicate-integer/submission-0.java
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -1,6 +1,9 @@
+// Pattern: HashSet
+// Idea: track seen elements
+
 class Solution {
     public boolean hasDuplicate(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();      // store numbers we've seen 
         for(int num :nums){
             if (seen.contains(num)){
                 return true;
